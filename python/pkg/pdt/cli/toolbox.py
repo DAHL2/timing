a58ed372dfef6fb3370a6b23ae8ea8beb085118b
Stoyan Trilov <stoyan.trilov@cern.ch>
2020-05-28 13:49:53 +0100

    consolidated code to establish ipmi connection to amc into a function

--- python/pkg/pdt/cli/toolbox.py
+++ python/pkg/pdt/cli/toolbox.py
@@ -368,6 +368,23 @@ def printCounters( aTopNode, aSubNodes, aNumCtrs=0x10, aTitle='Cmd', aLegend=def
 
 
 # ------------------------------------------------------------------------------
+def establishIPMIConnectionToAMC(mch_ip_adr,amc_slot):
+    amc_ipmb_addresses= [0x72, 0x74, 0x76, 0x78, 0x7a, 0x7c, 0x7e, 0x80, 0x82, 0x84, 0x86, 0x88]
+    amc_ipmb_adr = amc_ipmb_addresses[amc_slot-1]
+    
+    interface = pyipmi.interfaces.create_interface('ipmitool', interface_type='lan')
+    connection = pyipmi.create_connection(interface)
+
+    connection.target = pyipmi.Target(amc_ipmb_adr)
+    connection.target.set_routing([(0x81,0x20,0),(0x20,0x82,7),(0x20,amc_ipmb_adr,None)])
+    connection.session.set_session_type_rmcp(mch_ip_adr, port=623)
+    connection.session.set_auth_type_user('', '')
+    connection.session.establish()
+    return connection
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
 def readRegOverIPMI(ipmi_connection, reg):
     raw_read_cmd = b'\x00\x02\x4B\x01\x01'
     cmd = raw_read_cmd+struct.pack("B", reg)
@@ -520,9 +537,6 @@ def readGPIOPortOverIPMI(ipmi_connection, port):
     mode=b'\x00'
     cmd = raw_gpio_cmd+struct.pack("B", port)+mode
     
-    max_attempts=1
-    read_attempts=0
-
     cmd_result = []
     result = ipmi_connection.raw_command(0x00, 0x30, cmd)
 
